6efeca557f809e25faebec64c3e52f94189483f1
umarali222 <mohammadumaralikhan22@gmail.com>
2026-03-09 23:59:03 +0530

    Update BikePart schema to include status field

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db');git
 
 // Load environment variables
 dotenv.config();
